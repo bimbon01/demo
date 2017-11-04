4e8dbaba18279b9b398c7e83d7e205691afa687a
bimbon01 <letuanlinh92.hn@gmail.com>
2017-11-04 23:06:16 +0700

    <3

--- Demo/src/Demo.java
+++ Demo/src/Demo.java
@@ -1,10 +1,18 @@
+import java.util.*;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello wolrd !!!");
-		System.out.println("Linh Le");
+		// input value n
+		Scanner input_n = new Scanner(System.in);
+		System.out.println("Please, input the value of 'n' : ");
+		int n = input_n.nextInt();
+		
+		// print <3
+		for(int i = 0; i < n; i++) {
+			System.out.println("<3");
+		}
 	}
 
 }
